3ad36ff6843cd5ccb63c510b1a34f858bbc7ca83
linneszyx <itssuryanshcooldude346@gmail.com>
2026-04-22 18:26:05 +0530

    pdfGenerationComponent For VisualForce Task

---
+++ force-app/main/default/lwc/pdfGenerationComponent/pdfGenerationComponent.js
@@ -0,0 +1,18 @@
+import { LightningElement, api } from 'lwc';
+import generatePdf from '@salesforce/apex/PdfController.generatePdf';
+
+export default class PdfGenerationComponent extends LightningElement {
+    @api recordId;
+    isLoading = false;
+    async handleSave() {
+        this.isLoading = true;
+        try {
+            const docId = await generatePdf({ recordId: this.recordId });
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            this.isLoading = false;
+        }
+    }
+}
