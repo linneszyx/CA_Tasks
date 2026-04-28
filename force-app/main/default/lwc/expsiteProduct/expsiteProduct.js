e146599c44ba7f57d56d8b7602a38b7799a29817
linneszyx <itssuryanshcooldude346@gmail.com>
2026-04-28 11:47:31 +0530

    implemented the asset feature on My Asset Page

--- force-app/main/default/lwc/expsiteProduct/expsiteProduct.js
+++ force-app/main/default/lwc/expsiteProduct/expsiteProduct.js
@@ -6,30 +6,39 @@ export default class ExpsiteProduct extends LightningElement {
     rawData;
     error;
 
-    //  PRODUCT_ID = '012gL000004p2LdQAI';
-    //  SERVICE_ID = '012gL000004p2NFQAY';
-
     @wire(getAssetRT)
     wiredAssets({ data, error }) {
         console.log('Data ', data);
+        console.log('type :', this.type)
+        console.log('data  :', JSON.stringify(data))
+        console.log('error : ', JSON.stringify(error))
         if (data) {
             this.rawData = data;
+            console.log('Asset length :', data.al ? data.al.length : 'al is null')
+            console.log('rtMap : ', JSON.stringify(data.rtMap))
         } else if (error) {
             this.error = error;
             this.products = [];
             this.services = [];
-            // console.log('Error loading assets:', error);
+            console.log('Error loading assets:', error);
             console.error('Error', JSON.stringify(error, null, 2))
         }
     }
     get finalData() {
+        console.log('rawdata : ', this.rawData)
+        console.log('Type : ', this.type)
         if (!this.rawData) return []
         const asset = this.rawData.al;
         const mp = this.rawData.rtMap;
+        console.log('Mp Product : ', mp['Product'])
+        console.log('Mp Services : ', mp['Services'])
+        console.log('First Asset RecordTypeId : ', asset[0]?.RecordTypeId)
         const proRT = mp['Product']
         const serRT = mp['Services']
         const products = asset.filter(rec => rec.RecordTypeId === proRT);
         const services = asset.filter(rec => rec.RecordTypeId === serRT);
+        console.log('Product Count : ', products.length)
+        console.log('Services Count : ', services.length)
         return this.type === 'product' ?
             products : services;
     }
